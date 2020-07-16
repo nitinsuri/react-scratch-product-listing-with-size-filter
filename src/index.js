8bfbdede9e3ede9350986315182329cd0ab3f879
Nitin Suri <nitin.suri@transport.nsw.gov.au>
2020-07-16 11:41:36 +1000

    Woking on the context API

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from './js/App';
 import './scss/app';
 
+
 const wrapper = document.getElementById("container");
 wrapper ? ReactDOM.render(<App />, wrapper) : false;
--- src/js/state/Actions/Actions.js
+++
@@ -1,5 +0,0 @@
-import UPDATE_PRODUCTLIST from "../Constants/index";
-
-export function updateProductList(payload) {
-  return { type: "UPDATE_PRODUCTLIST", payload }
-};
